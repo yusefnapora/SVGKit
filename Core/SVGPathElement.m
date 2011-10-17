d77cc7c8e57d26faa7a46359a7288677c59aafef
Yusef Napora <yusef.napora@gmail.com>
2011-10-17 18:58:36 -0400

    Added support for quadratic curves.  Also added parsing for arcs, but no implementation as CoreGraphics only supports circular arc segments, while SVG allows elliptical arcs.  It may be possible to apply an affine transform to a circular arc segment to get the desired elliptical arc, but I'm too busy now.

--- Core/SVGPathElement.m
+++ Core/SVGPathElement.m
@@ -22,6 +22,8 @@
 - (void) moveTo:(CGPoint)point relative:(BOOL)relative;
 - (void) lineTo:(CGPoint)point relative:(BOOL)relative;
 - (void) addCurve:(CGPoint)control1 control2:(CGPoint)control2 endPoint:(CGPoint)endPoint;
+- (void) addQuadCurveToPoint:(CGPoint) endPoint controlPoint:(CGPoint)controlPoint;
+- (void) addArcToPoint:(CGPoint)endPoint theta:(CGFloat)theta radiusX:(CGFloat)radiusX radiusY:(CGFloat)radiusY largeArc:(BOOL)largeArc sweepArc:(BOOL)sweepArc; 
 - (void) closePath;
 
 @end
@@ -53,16 +55,17 @@
     [parserHelper skipWhitespace];
     self.CGPath = CGPathCreateMutable();
 
-    float lastX = 0;
-    float lastY = 0;
-    float lastX1 = 0;
-    float lastY1 = 0;
+    CGPoint lastPoint = CGPointZero;
+    CGPoint lastCurveControlPoint = CGPointZero;
+    CGPoint lastQuadCurveControlPoint = CGPointZero;
     
+        
     while (parserHelper.position < stringLength)
     {
         UniChar cmd = [string characterAtIndex:parserHelper.position];
         [parserHelper advance];
         BOOL wasCurve = NO;
+        BOOL wasQuadCurve = NO;
         switch (cmd)
         {
             case 'M':
@@ -73,11 +76,11 @@
                 BOOL relative = NO;
                 if (cmd == 'm') {
                     relative = YES;
-                    lastX += x;
-                    lastY += y;
+                    lastPoint.x += x;
+                    lastPoint.y += y;
                 } else {
-                    lastX = x;
-                    lastY = y;
+                    lastPoint.x = x;
+                    lastPoint.y = y;
                 }
                 [self moveTo:CGPointMake(x, y) relative:relative];
                 break;
@@ -98,11 +101,11 @@
                 BOOL relative = NO;
                 if (cmd == 'l') {
                     relative = YES;
-                    lastX += x;
-                    lastY += y;
+                    lastPoint.x += x;
+                    lastPoint.y += y;
                 } else {
-                    lastX = x;
-                    lastY = y;
+                    lastPoint.x = x;
+                    lastPoint.y = y;
                 }
                 [self lineTo:CGPointMake(x, y) relative:relative];
                 break;
@@ -114,10 +117,10 @@
                 float x = [parserHelper nextFloat];
                 if (cmd == 'h') {
                     [self lineTo:CGPointMake(x, 0) relative:YES];
-                    lastX += x;
+                    lastPoint.x += x;
                 } else {
-                    [self lineTo:CGPointMake(x, lastY) relative:NO];
-                    lastX = x;
+                    [self lineTo:CGPointMake(x, lastPoint.y) relative:NO];
+                    lastPoint.x = x;
                 }
                 break;
             }
@@ -128,10 +131,10 @@
                 float y = [parserHelper nextFloat];
                 if (cmd == 'v') {
                     [self lineTo:CGPointMake(0, y) relative:YES];
-                    lastY += y;
+                    lastPoint.y += y;
                 } else {
-                    [self lineTo:CGPointMake(lastX, y) relative:NO];
-                    lastY = y;
+                    [self lineTo:CGPointMake(lastPoint.x, y) relative:NO];
+                    lastPoint.y = y;
                 }
                 break;
             }
@@ -145,18 +148,16 @@
                 CGPoint pt = CGPointMake([parserHelper nextFloat], [parserHelper nextFloat]);
                 
                 if (cmd == 'c') {
-                    c1.x += lastX;
-                    c2.x += lastX;
-                    pt.x += lastX;
-                    c1.y += lastY;
-                    c2.y += lastY;
-                    pt.y += lastY;
+                    c1.x += lastPoint.x;
+                    c2.x += lastPoint.x;
+                    pt.x += lastPoint.x;
+                    c1.y += lastPoint.y;
+                    c2.y += lastPoint.y;
+                    pt.y += lastPoint.y;
                 }
                 [self addCurve:c1 control2:c2 endPoint:pt];
-                lastX1 = c2.x;
-                lastY1 = c2.y;
-                lastX = pt.x;
-                lastY = pt.y;
+                lastCurveControlPoint = c2;
+                lastPoint = pt;
                 
                 break;
             }
@@ -168,25 +169,81 @@
                 CGPoint c2 = CGPointMake([parserHelper nextFloat], [parserHelper nextFloat]);
                 CGPoint pt = CGPointMake([parserHelper nextFloat], [parserHelper nextFloat]);
                 if (cmd == 's') {
-                    c2.x += lastX;
-                    pt.x += lastX;
-                    c2.y += lastY;
-                    pt.y += lastY;
+                    c2.x += lastPoint.x;
+                    pt.x += lastPoint.x;
+                    c2.y += lastPoint.y;
+                    pt.y += lastPoint.y;
                 }
-                CGPoint c1 = CGPointMake(2 * lastX - lastX1, 2 * lastY - lastY1);
+                CGPoint c1 = CGPointMake(2 * lastPoint.x - lastCurveControlPoint.x, 2 * lastPoint.y - lastCurveControlPoint.y);
                 [self addCurve:c1 control2:c2 endPoint:pt];
-                lastX1 = c2.x;
-                lastY1 = c2.y;
-                lastX = pt.x;
-                lastY = pt.y;
+                lastCurveControlPoint = c2;
+                lastPoint = pt;
                 
+                break;
+            }
+            case 'Q':
+            case 'q':
+            {
+                wasQuadCurve = YES;
+                CGPoint controlPoint = CGPointMake([parserHelper nextFloat], [parserHelper nextFloat]);
+                CGPoint endPoint = CGPointMake([parserHelper nextFloat], [parserHelper nextFloat]);
+                if (cmd == 'q')
+                {
+                    controlPoint.x += lastPoint.x;
+                    controlPoint.y += lastPoint.y;
+                    endPoint.x += lastPoint.x;
+                    endPoint.y += lastPoint.y;
+                }
+                
+                [self addQuadCurveToPoint:endPoint controlPoint:controlPoint];
+                lastQuadCurveControlPoint = controlPoint;
+                lastPoint = endPoint;
+                break;
+            }
+            case 'T':
+            case 't':
+            {
+                wasQuadCurve = YES;
+                CGPoint endPoint = CGPointMake([parserHelper nextFloat], [parserHelper nextFloat]);
+                CGPoint controlPoint;
+                
+                controlPoint.x = 2 * lastPoint.x - lastQuadCurveControlPoint.x;
+                controlPoint.y = 2 * lastPoint.y - lastQuadCurveControlPoint.y;
+                if (cmd == 't') {
+                    endPoint.x += lastPoint.x;
+                    endPoint.y += lastPoint.y;
+                    controlPoint.x += lastPoint.x;
+                    controlPoint.y += lastPoint.y;
+                }
+                [self addQuadCurveToPoint:endPoint controlPoint:controlPoint];
+                lastQuadCurveControlPoint = controlPoint;
+                lastPoint = endPoint;
+                
+                break;
+            }
+            case 'A':
+            case 'a':
+            {
+                CGFloat rx = [parserHelper nextFloat];
+                CGFloat ry = [parserHelper nextFloat];
+                CGFloat theta = [parserHelper nextFloat];
+                BOOL largeArc = (BOOL)[parserHelper nextFloat];
+                BOOL sweepArc = (BOOL)[parserHelper nextFloat];
+                CGFloat x = [parserHelper nextFloat];
+                CGFloat y = [parserHelper nextFloat];
+                CGPoint pt = CGPointMake(x, y);
+                [self addArcToPoint:pt theta:theta radiusX:rx radiusY:ry largeArc:largeArc sweepArc:sweepArc];
+                lastPoint.x = x;
+                lastPoint.y = y;
                 break;
             }
         }
         
         if (!wasCurve) {
-            lastX1 = lastX;
-            lastY1 = lastY;
+            lastCurveControlPoint = lastPoint;
+        }
+        if (!wasQuadCurve) {
+            lastQuadCurveControlPoint = lastPoint;
         }
         [parserHelper skipWhitespace];
     }
@@ -230,6 +287,16 @@
 - (void) addCurve:(CGPoint)control1 control2:(CGPoint)control2 endPoint:(CGPoint)endPoint
 {
     CGPathAddCurveToPoint(self.CGPath, NULL, control1.x, control1.y, control2.x, control2.y, endPoint.x, endPoint.y);
+}
+
+- (void) addQuadCurveToPoint:(CGPoint) endPoint controlPoint:(CGPoint)controlPoint
+{
+    
+}
+
+- (void) addArcToPoint:(CGPoint)endPoint theta:(CGFloat)theta radiusX:(CGFloat)radiusX radiusY:(CGFloat)radiusY largeArc:(BOOL)largeArc sweepArc:(BOOL)sweepArc; 
+{
+    // Not yet implemented
 }
 
 - (void) closePath
